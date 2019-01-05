f95b289bc9c7f1359618a08fb5c5fa8b0d9aceca
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2019-01-05 23:57:21 +0100

    added loss_cross_entropy

--- tests/testthat/helper-objects.R
+++ tests/testthat/helper-objects.R
@@ -6,3 +6,9 @@ explainer_glm <- explain(HR_glm_model, data = HR,  y = HR$status == "fired")
 
 HR_rf_model <- randomForest(status ~ ., data = HR, ntree = 100)
 explainer_rf  <- explain(HR_rf_model, data = HR, y = HR$status)
+
+loss_cross_entropy <- function (observed, predicted, p_min = 0.0001) {
+  p <- sapply(seq_along(observed), function(i) predicted[i, observed[i]])
+  sum(-log(pmax(p, p_min)))
+}
+
