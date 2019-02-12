fb0a9555afc55197a243df48f27b14149a061e7d
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2019-02-12 15:02:37 +0100

    show functions extracted with spaghetti like plot function in the ceterisParibus2

---
+++ R/show_rugs.R
@@ -0,0 +1,80 @@
+#' Adds a Layer with Rugs to a Profile Plot
+#'
+#' Function 'show_rugs' adds a layer to a plot created with 'plot.ceteris_paribus_explainer' for selected observations.
+#' Various parameters help to decide what should be plotted, profiles, aggregated profiles, points or rugs.
+#'
+#' @param x a ceteris paribus explainer produced with function `ceteris_paribus()`
+#' @param ... other explainers that shall be plotted together
+#' @param color a character. Either name of a color or name of a variable that should be used for coloring
+#' @param size a numeric. Size of lines to be plotted
+#' @param sides a string containing any of "trbl", for top, right, bottom, and left. Passed to geom rug.
+#' @param alpha a numeric between 0 and 1. Opacity of lines
+#' @param selected_variables if not NULL then only `selected_variables` will be presented
+#' @param only_numerical a logical. If TRUE then only numerical variables will be plotted. If FALSE then only categorical variables will be plotted.
+#'
+#' @return a ggplot2 layer
+#' @export
+show_rugs <- function(x, ...,
+                      size = 0.5,
+                      alpha = 1,
+                      color = "black",
+
+                      only_numerical = TRUE,
+                      sides = "b",
+
+                      selected_variables = NULL) {
+
+  # if there is more explainers, they should be merged into a single data frame
+  dfl <- c(list(x), list(...))
+
+  all_observations <- lapply(dfl, function(tmp) {
+    attr(tmp, "observations")
+  })
+  all_observations <- do.call(rbind, all_observations)
+  all_observations$`_ids_` <- factor(rownames(all_observations))
+
+  # variables to use
+  all_variables <- grep(colnames(all_observations), pattern = "^[^_]", value = TRUE)
+  if (!is.null(selected_variables)) {
+    all_variables <- intersect(all_variables, selected_variables)
+    if (length(all_variables) == 0) stop(paste0("selected_variables do not overlap with ", paste(all_variables, collapse = ", ")))
+  }
+  # only numerical or only factors?
+  is_numeric <- sapply(all_observations[, all_variables, drop = FALSE], is.numeric)
+  if (only_numerical) {
+    vnames <- all_variables[which(is_numeric)]
+    if (length(vnames) == 0) stop("There are no numerical variables")
+  } else {
+    vnames <- all_variables[which(!is_numeric)]
+    if (length(vnames) == 0) stop("There are no non-numerical variables")
+  }
+
+  # prepare data for plotting points
+  is_color_points_a_variable    <- color %in% c(all_variables, "_label_", "_vname_", "_ids_")
+
+  tmp <- lapply(vnames, function(var) {
+    data.frame(`_x_` = all_observations[,var],
+               `_vname_` = var,
+               `_yhat_`  = all_observations$`_yhat_`,
+               `_y_`     = if (is.null(all_observations$`_y_`)) NA else all_observations$`_y_`,
+               `_color_` = if (!is_color_points_a_variable) NA else {
+                 if (color == "_vname_") var else all_observations[,color]
+               },
+               `_ids_`   = all_observations$`_ids_`,
+               `_label_`  = all_observations$`_label_`)
+  })
+  all_observations_long <- do.call(rbind, tmp)
+  colnames(all_observations_long) <- c("_x_", "_vname_", "_yhat_", "_y_", "_color_", "_ids_", "_label_")
+  if ((is_color_points_a_variable ) & !(color %in% colnames(all_observations_long)))
+    colnames(all_observations_long)[5] = color
+
+  # show rugs
+  if (is_color_points_a_variable) {
+    res <- geom_rug(data = all_observations_long, aes_string(color = paste0("`",color,"`")), size = size, alpha = alpha, sides = sides)
+  } else {
+    res <- geom_rug(data = all_observations_long, size = size, alpha = alpha, color = color, sides = sides)
+  }
+
+  res
+}
+
