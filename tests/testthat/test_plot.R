83eb13532e1474a500899f36f053fe45c45dd42e
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2019-02-13 17:34:24 +0100

    updated tests

--- tests/testthat/test_plot.R
+++ tests/testthat/test_plot.R
@@ -1,7 +1,7 @@
 context("Check plot() functions")
 
-test_that("plot model_feature_importance_explainer",{
-  vd_glm <- model_feature_importance(explainer_glm, type = "raw",
+test_that("plot feature_importance_explainer",{
+  vd_glm <- feature_importance(explainer_glm, type = "raw",
                                      loss_function = loss_root_mean_square)
   pl <- plot(vd_glm)
   expect_true("gg" %in% class(pl))
--- tests/testthat/test_variable_dropout.R
+++ tests/testthat/test_variable_dropout.R
@@ -1,14 +1,14 @@
-context("Check model_feature_importance() function")
+context("Check feature_importance() function")
 
 test_that("Output glm",{
-  vd_glm <- model_feature_importance(explainer_glm, type = "raw",
+  vd_glm <- feature_importance(explainer_glm, type = "raw",
                                      loss_function = loss_root_mean_square)
-  expect_true("model_feature_importance_explainer" %in% class(vd_glm))
+  expect_true("feature_importance_explainer" %in% class(vd_glm))
 })
 
 test_that("Output rf",{
-  vd_rf <- model_feature_importance(explainer_rf,
+  vd_rf <- feature_importance(explainer_rf,
                                      loss_function = loss_cross_entropy)
-  expect_true("model_feature_importance_explainer" %in% class(vd_rf))
+  expect_true("feature_importance_explainer" %in% class(vd_rf))
 })
 
