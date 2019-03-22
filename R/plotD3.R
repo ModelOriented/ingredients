7d7f8d211f4ab3d11c036660d34fcf45acde2ea6
hbaniecki <h.baniecki@student.mini.pw.edu.pl>
2019-03-22 14:35:12 +0100

    update plotD3
    changes made:
    - fixed bar plotting when variable dropout is smaller than _full_model_
    - updated bar sorting in groups
    - changed argument names and example
    - updated documentation
    - changed js file name

--- R/plotD3.R
+++ R/plotD3.R
@@ -1,32 +1,40 @@
 #' Plot Feature Importance Objects in D3 with r2d3 package.
 #'
-#' @param x the model model of `feature_importance_explainer` class.
-#' @param ... other parameters.
-#' @param label default TRUE
-#' @param scaleHeight default FALSE
+#' Function \code{plotD3.feature_importance_explainer} plots dropouts for variables used in the model.
+#' It uses output from \code{feature_importance} function that corresponds to permutation based measure of feature importance.
+#' Variables are sorted in the same order in all panels. The order depends on the average drop out loss. In different panels variable contributions may not look like sorted if variable importance is different in different models.
+#'
+#' @param x a feature importance explainer produced with the 'feature_importance' function
+#' @param ... other explainers that shall be plotted together
+#' @param split either \code{"model"} or \code{"feature"} determines the plot layout
+#' @param scale_height should the height of plot scale with window size? By default it's FALSE
 #'
 #' @return an `r2d3` object.
 #'
 #' @examples
 #' \dontrun{
 #' ## Not run:
-#' set.seed(59)
 #' library("DALEX")
+#' library("ingredients")
 #' library("caret")
 #'
-#' regr_rf <- train(m2.price~., data = apartments, method="rf", ntree = 100)
-#' regr_svm <- train(m2.price~., data = apartments, method="svmLinear")
-#' explainer_rf <- explain(regr_rf, data = apartmentsTest[,2:6],
-#' y = apartmentsTest$m2.price, label="rf")
-#' explainer_svm <- explain(regr_svm, data = apartmentsTest[,2:6],
-#' y = apartmentsTest$m2.price, label="svm")
+#' rf_model <- train(m2.price~., data = apartments, method="rf", ntree = 100)
+#' explainer_rf <- explain(rf_model, data = apartments_test[,2:6],
+#'                         y = apartments_test$m2.price, label="rf")
 #' fi_rf <- feature_importance(explainer_rf, loss_function = loss_root_mean_square)
+#'
+#' head(fi_rf)
+#' plotD3(fi_rf)
+#'
+#' svm_model <- train(m2.price~., data = apartments, method="svmLinear")
+#' explainer_svm <- explain(svm_model, data = apartments_test[,2:6],
+#'                         y = apartments_test$m2.price, label="svm")
 #' fi_svm <- feature_importance(explainer_svm, loss_function = loss_root_mean_square)
 #'
+#' head(fi_svm)
 #' plotD3(fi_rf, fi_svm)
-#' plotD3(fi_rf, fi_svm, scaleHeight = TRUE)
-#' plotD3(fi_rf, fi_svm, label = FALSE)
-#' plotD3(fi_rf, fi_svm, label = FALSE, scaleHeight = TRUE)
+#'
+#' plotD3(fi_rf, fi_svm, split = "feature")
 #' }
 #' @export
 #' @rdname plotD3
@@ -36,7 +44,10 @@ plotD3 <- function(x, ...)
 
 #' @export
 #' @rdname plotD3
-plotD3.feature_importance_explainer <-  function(x, ..., label = TRUE, scaleHeight = FALSE){
+plotD3.feature_importance_explainer <-  function(x, ..., split = "model", scale_height = FALSE){
+    if (!(split %in% c("model", "feature"))){
+      stop("The plotD3.feature_importance_explainer() function requires split to be model or feature.")
+    }
 
     n <- length(list(...)) + 1
     m <- dim(x)[1]
@@ -52,22 +63,20 @@ plotD3.feature_importance_explainer <-  function(x, ..., label = TRUE, scaleHeig
     bestFits <- df[df$variable == "_full_model_", ]
     df <- merge(df, bestFits[,c("label", "dropout_loss")], by = "label")
 
-    perm <- aggregate(df$dropout_loss.x, by=list(Category=df$variable), FUN=mean)
+    perm <- aggregate(df$dropout_loss.x, by = list(Category=df$variable), FUN = mean)
 
-    options <- list(xmin = xmin, xmax = xmax, ticksMargin=ticksMargin, scaleHeight=scaleHeight)
+    options <- list(xmin = xmin, xmax = xmax, ticksMargin = ticksMargin, scaleHeight = scale_height)
 
-    if (label == TRUE){
+    if (split == "model"){
       # one plot for each model
 
       # sorting bars in groups
-      adding <- c()
-      for (i in 1:n){
-        adding <- c(adding, rep((i-1)*m,m))
-      }
-      perm <- match(df$variable, perm$Category[order(perm$x)]) + adding
+      perm <- perm$Category[order(perm$x)]
+      df$variable <- factor(as.character(df$variable), levels = as.character(perm))
+      df <- df[order(df$label, df$variable),]
 
       colnames(df) <- c("label","variable","dropout_loss")
-      temp <- split(df[perm,2:3], f = df$label)
+      temp <- split(df[,2:3], f = df$label)
       temp <- jsonlite::toJSON(temp)
 
       # n - number of models, m - number of features
@@ -78,7 +87,7 @@ plotD3.feature_importance_explainer <-  function(x, ..., label = TRUE, scaleHeig
            d3_version = 4,
            options = options)
 
-    } else {
+    } else if (split == "feature"){
       # one plot for each feature
 
       colnames(df) <- c("label","variable","dropout_loss")
@@ -90,7 +99,7 @@ plotD3.feature_importance_explainer <-  function(x, ..., label = TRUE, scaleHeig
       options["n"] <- m
       options["m"] <- n
 
-      r2d3::r2d3(data = temp, script = system.file("featureImportance2.js", package = "ingredients"),
+      r2d3::r2d3(data = temp, script = system.file("featureImportanceSplit.js", package = "ingredients"),
            d3_version = 4,
            options = options)
     }
--- inst/featureImportance.js
+++ inst/featureImportance.js
@@ -1,5 +1,5 @@
 /*
-TODO:add all css stuff 
+TODO:add all css stuff
 */
 var minValue = options.xmin;
 var maxValue = options.xmax;
@@ -16,18 +16,18 @@ var labelsMargin = margin.left - 6;
 var plotTop = margin.top;
 
 if (options.scaleHeight === true) {
-  plotHeight = (h-(n-1)*margin.inner)/n; 
+  plotHeight = (h-(n-1)*margin.inner)/n;
 } else {
   plotHeight = m*25;
 }
 
 var plotBottom = margin.top + plotHeight;
-          
+
 featureImportance(data);
 
 // css
-var tooltip = d3.select("body").append("div")	
-          .attr("class", "tooltip")				
+var tooltip = d3.select("body").append("div")
+          .attr("class", "tooltip")
           .style("position", "absolute")
           .style("text-align", "center")
           .style("width", "100px")
@@ -38,17 +38,17 @@ var tooltip = d3.select("body").append("div")
           .style("border-radius", "8px")
           .style("pointer-events", "none")
           .style("visibility", "hidden");
-          
+
 svg.selectAll(".grid").selectAll(".tick line")
     .style("stroke", "lightgrey")
     .style("stroke-opacity", "0.7")
     .style("shape-rendering", "crispEdges");
 svg.selectAll(".fullModel").style("font-weight", "bold");
-    
+
 // plot function
 function featureImportance(data) {
     var i = 1;
-    
+
     for (var j in data) {
       var modelName = j;
       var modelData = data[j];
@@ -60,16 +60,16 @@ function featureImportance(data) {
 function singleFeatureImportance(modelName, modelData, i) {
 
     var colors = ["#4378bf", "#46bac2", "#371ea3", "#8bdcbe", "#ae2c87", "#ffa58c", "#f05a71"];
-    
+
     var x = d3.scaleLinear()
         .range([margin.left, w + margin.left])
         .domain([minValue - ticksMargin, maxValue + ticksMargin]);
-        
+
     if (i != 1){
       plotTop += margin.inner + plotHeight;
-      plotBottom += margin.inner + plotHeight;  
-    }    
-    
+      plotBottom += margin.inner + plotHeight;
+    }
+
     if (i == 1){
       // title
         svg.append("text")
@@ -80,21 +80,21 @@ function singleFeatureImportance(modelName, modelData, i) {
           .style("text-anchor", "left")
           .text("Feature importance");
     }
-    
+
     if (i == n){
       // xaxis
-        svg.append("text")             
+        svg.append("text")
           .attr("transform",
-                "translate(" + (width/2) + " ," + 
+                "translate(" + (width/2) + " ," +
                                (margin.top + n*(margin.inner+plotHeight)) + ")")
           .style("font-size", "16px")
           .style("text-anchor", "left")
           .text("Drop-out loss");
-        
+
         var xAxis = d3.axisBottom(x)
                     .ticks(5)
                     .tickSize(0);
-                  
+
         xAxis = svg.append("g")
           .attr("class", "xAxis")
           .attr("transform", "translate(0," + plotBottom + ")")
@@ -102,14 +102,14 @@ function singleFeatureImportance(modelName, modelData, i) {
           .call(xAxis)
           .call(g => g.select(".domain").remove());
     }
-    
+
     var y = d3.scaleBand()
         .rangeRound([plotBottom, plotTop])
         .padding(0.35)
         .domain(modelData.map(function (d) {
              return d.variable;
         }));
-        
+
     // modelname
     svg.append("text")
         .attr("x", margin.left )
@@ -118,53 +118,55 @@ function singleFeatureImportance(modelName, modelData, i) {
         .style("font-weight", "bold")
         .style("text-anchor", "left")
         .text(modelName);
-    
+
     // grid
-    var xGrid = svg.append("g")			
+    var xGrid = svg.append("g")
          .attr("class", "grid")
          .attr("transform", "translate(0," + plotBottom + ")")
          .call(d3.axisBottom(x)
-                .ticks(10) 
+                .ticks(10)
                 .tickSize(-plotHeight)
                 .tickFormat("")
         ).call(g => g.select(".domain").remove());
-    
-    var yGrid = svg.append("g")			
+
+    var yGrid = svg.append("g")
          .attr("class", "grid")
          .attr("transform", "translate(" + margin.left + ",0)")
          .call(d3.axisLeft(y)
                 .tickSize(-w)
                 .tickFormat("")
         ).call(g => g.select(".domain").remove());
-    
+
     // yaxis
     var yAxis = d3.axisLeft(y)
         .tickSize(0);
-        
+
     yAxis = svg.append("g")
         .attr("class", "yAxis")
         .attr("transform","translate(" + labelsMargin + ",0)")
         .style("font-size", "13px")
         .call(yAxis)
         .call(g => g.select(".domain").remove());
-    
+
     // tag full model to later make it bold
     yAxis.selectAll("text").each(function() {
         if(this.textContent === "_full_model_") {
           this.classList.add("fullModel");
         }
       });
-    
+
     // bars
     var bars = svg.selectAll()
         .data(modelData)
         .enter()
         .append("g");
-        
-    var barStart = d3.min(modelData, function (d) {
-            return x(d.dropout_loss);
-        });
-        
+
+    // find full model dropout_loss value
+    var barStart;
+    modelData.forEach((item) => {
+      if(item.variable==="_full_model_") barStart = x(item.dropout_loss);
+      });
+
     bars.append("rect")
         .attr("class", modelName)
         .attr("fill", colors[i])
@@ -172,32 +174,39 @@ function singleFeatureImportance(modelName, modelData, i) {
             return y(d.variable);
         })
         .attr("height", y.bandwidth())
-        .attr("x", barStart)
+        .attr("x", function (d) {
+          // start ploting the bar left to full model line
+          if (x(d.dropout_loss) < barStart) {
+            return x(d.dropout_loss);
+          } else {
+            return barStart;
+          }
+        })
         .attr("width", function (d) {
-            return x(d.dropout_loss) - barStart;
+            return  Math.abs(x(d.dropout_loss) - barStart);
         });
-    
+
     var barMin = d3.min(modelData, function (d) {
             return d.dropout_loss;
         });
-        
+
     // tooltip functions
     bars.on("mouseover", function(){
             tooltip.style("visibility", "visible");
         })
-        .on("mousemove", function(d) {		
-            tooltip .html( 
-              Math.round(d.dropout_loss * 100)/100 
+        .on("mousemove", function(d) {
+            tooltip .html(
+              Math.round(d.dropout_loss * 100)/100
               + "</br>" + "+"
-              + Math.round((d.dropout_loss-barMin) * 100)/100 
+              + Math.round((d.dropout_loss-barMin) * 100)/100
             )
-                .style("left", (d3.event.pageX) + "px")		
-                .style("top", (d3.event.pageY - 28) + "px");	
-        })					
-        .on("mouseout", function(d) {		
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+        })
+        .on("mouseout", function(d) {
             tooltip.style("visibility", "hidden");
         });
-        
+
     // make line next to bars
     var minimumY = Number.MAX_VALUE;
     var maximumY = Number.MIN_VALUE;
@@ -209,11 +218,11 @@ function singleFeatureImportance(modelName, modelData, i) {
         maximumY = +this.getAttribute('y');
       }
     });
-    
-    svg.append("line")          
-        .style("stroke", "red")  
-        .attr("x1", barStart)     
-        .attr("y1", minimumY)      
-        .attr("x2", barStart)     
-        .attr("y2", maximumY + y.bandwidth()); 
+
+    svg.append("line")
+        .style("stroke", "red")
+        .attr("x1", barStart)
+        .attr("y1", minimumY)
+        .attr("x2", barStart)
+        .attr("y2", maximumY + y.bandwidth());
 }
