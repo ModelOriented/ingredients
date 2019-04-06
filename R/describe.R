0347c342eb846e83ecd52d449d47a4435698fb6b
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2019-04-06 17:23:27 +0200

    candidate fix for #12

--- R/describe.R
+++ R/describe.R
@@ -9,8 +9,41 @@
 #' @return an `ceteris_paribus_descriptions` object.
 #'
 #' @examples
-#' \dontrun{
-#' }
+#' library("DALEX")
+#' # Toy examples, because CRAN angels ask for them
+#' titanic <- na.omit(titanic)
+#' model_titanic_glm <- glm(survived == "yes" ~ gender + age + fare,
+#'                        data = titanic, family = "binomial")
+#'
+#' explain_titanic_glm <- explain(model_titanic_glm,
+#'                            data = titanic[,-9],
+#'                            y = titanic$survived == "yes")
+#' cp_rf <- ceteris_paribus(explain_titanic_glm, titanic[1,])
+#' cp_rf
+#' describe(cp_rf)
+#'
+#' plot(cp_rf, variables = "age")
+#'
+#'  \donttest{
+#'  library("randomForest")
+#'  model_titanic_rf <- randomForest(survived == "yes" ~ gender + age + class + embarked +
+#'                                     fare + sibsp + parch,  data = titanic)
+#'  model_titanic_rf
+#'
+#'  explain_titanic_rf <- explain(model_titanic_rf,
+#'                            data = titanic[,-9],
+#'                            y = titanic$survived == "yes",
+#'                            label = "Random Forest v7")
+#'
+#' selected_passangers <- select_sample(titanic, n = 100)
+#' cp_rf <- ceteris_paribus(explain_titanic_rf, selected_passangers)
+#' cp_rf
+#' describe(cp_rf)
+#'
+#' plot(cp_rf, variables = "age") +
+#' show_observations(cp_rf, variables = "age") +
+#'   show_rugs(cp_rf, variables = "age", color = "red")
+#'  }
 #' @export
 #' @rdname describe
 
@@ -23,17 +56,16 @@ describe.ceteris_paribus_explainer <- function(x, ...) {
   # split ceteris_paribus_explainer into groups
   split_x <- split(x, x$`_vname_`)
 
-  original_x <- attr(sp_rf, "observations")
-  original_yhat <- original_x$`_yhat_`
+  original_x <- attr(x, "observations")
 
   descriptions <- lapply(split_x, function(x_part) {
     vname <- as.character(head(x_part$`_vname_`, 1))
 
     if (class(x_part[,vname]) == "numeric") {
-      cutpoint <- find_optimal_cutpoint( x_part$`_yhat_`)
+      cutpoint <- find_optimal_cutpoint(x_part$`_yhat_`)
       sufix <- describe_numeric_variable(original_x, x_part, cutpoint, vname)
     } else {
-      sufix <- ""
+      sufix <- describe_factor_variable(original_x, x_part, vname)
     }
 
     # prefix
@@ -42,20 +74,31 @@ describe.ceteris_paribus_explainer <- function(x, ...) {
     paste(prefix, sufix)
   })
 
+  class(descriptions) = "ceteris_paribus_descriptions"
   descriptions
 }
 
 
 find_optimal_cutpoint <- function(x) {
-  diffs <- sapply(1:(length(x)-1), function(cutpoint) {
+  diffs <- sapply(1:(length(x) - 1), function(cutpoint) {
     abs(mean(x[1:cutpoint]) - mean(x[-(1:cutpoint)]))
   })
   w <- pmin(seq_along(diffs) / length(diffs), 1 - seq_along(diffs) / length(diffs))
   which.max(diffs * w^0.25)
 }
 
-describe_factor_variable <- function() {
-
+describe_factor_variable <- function(original_x, x_part, vname) {
+  sorted_x_part <- x_part[order(x_part$`_yhat_`),]
+  sorted_id <- which(sorted_x_part[,vname] == as.character(original_x[,vname]))
+  if (median(x_part$`_yhat_`) < original_x$`_yhat_`) {
+    # x is higher than average, show average
+    levs <- rev(as.character(sorted_x_part[1:(sorted_id - 1), vname]))
+    sufix <- paste0("Model results higher values for: ", paste(levs, collapse = ", "), " \n")
+  } else {
+    levs <- as.character(sorted_x_part[(sorted_id+1):nrow(sorted_x_part), vname])
+    sufix <- paste0("Model results higher values for: ", paste(levs, collapse = ", "), " \n")
+  }
+  sufix
 }
 
 describe_numeric_variable <- function(original_x, x_part, cutpoint, vname) {
@@ -63,16 +106,16 @@ describe_numeric_variable <- function(original_x, x_part, cutpoint, vname) {
   if (original_x[1, vname] <= x_part[cutpoint, vname]) {
     # point is higher than the average
     if (mean(x_part[1:cutpoint, vname]) > mean(x_part[, vname])) {
-      sufix = paste0("For higher values of ", vname, " model responses are lower\n")
+      sufix = paste0("Model responses are *lower* for *higher* values of ", vname, " \n")
     } else {
-      sufix = paste0("For higher values of ", vname, " model responses are higher\n")
+      sufix = paste0("Model responses are *higher* for *higher* values of ", vname, " \n")
     }
   } else {
     # point is higher than the average
     if (mean(x_part[1:cutpoint, vname]) > mean(x_part[, vname])) {
-      sufix = paste0("For lower values of ", vname, " model responses are higher\n")
+      sufix = paste0("Model responses are *higher* for *lower* values of ", vname, " \n")
     } else {
-      sufix = paste0("For lower values of ", vname, " model responses are lower\n")
+      sufix = paste0("Model responses are *lower* for *lower* values of ", vname, " \n")
     }
   }
   sufix
--- R/print_ceteris_paribus_descriptions.R
+++ R/print_ceteris_paribus_descriptions.R
@@ -7,8 +7,41 @@
 #'
 #' @examples
 #' library("DALEX")
+#' # Toy examples, because CRAN angels ask for them
+#' titanic <- na.omit(titanic)
+#' model_titanic_glm <- glm(survived == "yes" ~ gender + age + fare,
+#'                        data = titanic, family = "binomial")
+#'
+#' explain_titanic_glm <- explain(model_titanic_glm,
+#'                            data = titanic[,-9],
+#'                            y = titanic$survived == "yes")
+#' cp_rf <- ceteris_paribus(explain_titanic_glm, titanic[1,])
+#' cp_rf
+#' describe(cp_rf)
+#'
+#' plot(cp_rf, variables = "age")
+#'
 #'  \donttest{
-#' }
+#'  library("randomForest")
+#'  model_titanic_rf <- randomForest(survived == "yes" ~ gender + age + class + embarked +
+#'                                     fare + sibsp + parch,  data = titanic)
+#'  model_titanic_rf
+#'
+#'  explain_titanic_rf <- explain(model_titanic_rf,
+#'                            data = titanic[,-9],
+#'                            y = titanic$survived == "yes",
+#'                            label = "Random Forest v7")
+#'
+#' selected_passangers <- select_sample(titanic, n = 100)
+#' cp_rf <- ceteris_paribus(explain_titanic_rf, selected_passangers)
+#' cp_rf
+#' describe(cp_rf)
+#'
+#' plot(cp_rf, variables = "age") +
+#' show_observations(cp_rf, variables = "age") +
+#'   show_rugs(cp_rf, variables = "age", color = "red")
+#'  }
+#'
 print.ceteris_paribus_descriptions <- function(x, ...) {
   for (element in x) {
     cat(element, "\n")
