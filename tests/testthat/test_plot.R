17eeeaca1dad38e67357bfd718b2ac3b75e6dfca
Hubert Baniecki <32574004+hbaniecki@users.noreply.github.com>
2019-07-16 15:43:14 +0200

    final attempt

--- tests/testthat/test_plot.R
+++ tests/testthat/test_plot.R
@@ -6,15 +6,14 @@ test_that("plot feature_importance_explainer",{
   pl <- plot(vd_glm)
   expect_true("gg" %in% class(pl))
 
-  vd_glm <- feature_importance(explainer_glm, type = "raw",
-                               loss_function = loss_root_mean_square)
   pl2 <- plot(vd_glm, max_vars = 3)
   expect_equal(nrow(pl2$data), 3)
 })
 
 test_that("plot ceteris_paribus_oscillations", {
-  cp_glm <- ceteris_paribus(explainer_glm, titanic[10:12, ])
-  os_glm <- calculate_oscillations(cp_glm)
-  pl <- plot(os_glm)
+  cp_rf <- ceteris_paribus(explainer_rf, titanic_small[10:12, ])
+  os_rf <- calculate_oscillations(cp_rf)
+  pl <- plot(os_rf)
   expect_true("gg" %in% class(pl))
 })
+
