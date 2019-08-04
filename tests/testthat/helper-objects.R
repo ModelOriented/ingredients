b8f1881daac2688f31ac5a2d07c2e1310024aff6
Katarzyna Pekala <git@pekala.net>
2019-08-04 13:50:14 +0200

    updated test coverage and fixed #41

--- tests/testthat/helper-objects.R
+++ tests/testthat/helper-objects.R
@@ -25,3 +25,37 @@ rf_model <- randomForest(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + 
 explainer_rf <- explain(rf_model, data = titanic_small,
                         y = titanic_small$Survived == "1", label = "RF")
 
+# helper objects for aspect_importance tests
+titanic_data <- na.omit(titanic)
+titanic_glm_model <- glm(survived == "yes" ~ class+gender+age+sibsp+parch+fare+embarked,
+                         titanic_data, family = "binomial")
+
+titanic_new_observation <- data.frame(
+  class = factor("1st", levels = c("1st", "2nd", "3rd", "deck crew",
+                                   "engineering crew", "restaurant staff",
+                                   "victualling crew")),
+  gender = factor("male", levels = c("female", "male")),
+  age = 8,
+  sibsp = 0,
+  parch = 0,
+  fare = 72,
+  embarked = factor("Southampton", levels = c("Belfast","Cherbourg",
+                                              "Queenstown","Southampton"))
+)
+
+titanic_aspects <- list(wealth = c("class", "fare"),
+                        family = c("gender", "sibsp", "parch"),
+                        age = "age",
+                        embarked = "embarked")
+
+
+apartments_lm_model <- lm(m2.price ~ ., data = apartments)
+
+apartments_aspects <- list(space = c("surface", "no.rooms"),
+                           construction.year = "construction.year",
+                           floor = "floor",
+                           district = "district")
+
+apartments_new_observation <- apartments_test[2,-1]
+
+
