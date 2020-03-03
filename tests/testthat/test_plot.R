58ae3e9d25234768dd950aa75320daded1eb8ede
hbaniecki <h.baniecki@student.mini.pw.edu.pl>
2020-03-03 16:29:17 +0100

    travisplz

--- tests/testthat/test_plot.R
+++ tests/testthat/test_plot.R
@@ -2,8 +2,6 @@ context("Check plot() functions")
 
 vd_glm <- feature_importance(explainer_glm, type = "raw",
                              loss_function = loss_root_mean_square)
-pl2 <- plot(vd_glm, max_vars = 3)
-cat(nrow(pl2$data), pl2[["labels"]][["subtitle"]])
 
 test_that("plot feature_importance_explainer",{
   pl <- plot(vd_glm)
@@ -12,6 +10,9 @@ test_that("plot feature_importance_explainer",{
 
 test_that("plot fi 2", {
   pl2 <- plot(vd_glm, max_vars = 3)
+
+  cat(nrow(pl2$data), pl2[["labels"]][["subtitle"]])
+
   expect_equal(nrow(pl2$data), 3)
   expect_equal(pl2[["labels"]][["subtitle"]], "created for the lm model")
 })
