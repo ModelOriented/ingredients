cebb78f381ce02e9f4b81c750b331ad873066d7d
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2020-07-28 21:42:55 +0200

    candidate fox for https://github.com/ModelOriented/ingredients/issues/122

--- R/plot_aggregated_profiles.R
+++ R/plot_aggregated_profiles.R
@@ -6,7 +6,7 @@
 #'
 #' @param x a ceteris paribus explainer produced with function \code{aggregate_profiles()}
 #' @param ... other explainers that shall be plotted together
-#' @param color a character. Either name of a color or name of a variable that should be used for coloring
+#' @param color a character. Either name of a color, or hex code for a color, or \code{_label_} if models shall be colored, or \code{_ids_} if instances shall be colored
 #' @param size a numeric. Size of lines to be plotted
 #' @param alpha a numeric between \code{0} and \code{1}. Opacity of lines
 #' @param facet_ncol number of columns for the \code{\link[ggplot2]{facet_wrap}}
